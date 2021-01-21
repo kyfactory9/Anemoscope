7fa1a5b71c3f13f89974fb76406b6fccd4da51c8
kyfactory <hyperhand9@gmail.com>
2021-01-21 22:50:35 +0900

    Simple Anemoscope

---
+++ WpfApp1/Anemoscope.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Anemoscope.xaml
+    /// </summary>
+    public partial class Anemoscope : UserControl
+    {
+        public static readonly DependencyProperty ScopeColorProperty = DependencyProperty.Register("ScopeColor", typeof(Brush), typeof(Anemoscope), new FrameworkPropertyMetadata(Brushes.Green));
+        public static readonly DependencyProperty WindDirectionProperty = DependencyProperty.Register("WindDirection", typeof(double), typeof(Anemoscope),
+            new FrameworkPropertyMetadata(0.0, new PropertyChangedCallback(WindDirectionChanged)));
+
+        public Brush ScopeColor
+        {
+            get { return (Brush)GetValue(ScopeColorProperty); }
+            set { SetValue(ScopeColorProperty, value); }
+        }
+
+        public double WindDirection
+        {
+            get { return (double)GetValue(WindDirectionProperty); }
+            set { SetValue(WindDirectionProperty, value); }
+        }
+
+        public Anemoscope()
+        {
+            InitializeComponent();
+        }
+
+        public static void WindDirectionChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            Anemoscope control = (Anemoscope)sender;
+            control.RotateNiddle((double)e.OldValue, (double)e.NewValue);
+        }
+
+        private void RotateNiddle(double oldValue, double newValue)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = oldValue;
+            animation.To = newValue;
+            animation.Duration = new Duration(TimeSpan.FromMilliseconds(500));
+            rotate.BeginAnimation(RotateTransform.AngleProperty, animation);
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 3);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int angle = r.Next(0, 359);
+            anemoscope.WindDirection = angle;
+        }
+    }
+}
